4ed9aec8989d1b338174e5621a09d097d5278f44
HassanHamdouna <hassan.h.developer@gmail.com>
2023-07-26 00:26:28 +0300

    add file firebase_options

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:auth_app/firebase_options.dart';
 import 'package:auth_app/screens/app/home_screen.dart';
 import 'package:auth_app/screens/auth/core/launch_screen.dart';
 import 'package:auth_app/screens/auth/login_screen.dart';
 import 'package:auth_app/screens/auth/register_phone_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
